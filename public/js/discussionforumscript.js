99c5c8a7ee313e57a0cb09ed11a4d0b1a24063fa
mengli150 <lmengo515@gmail.com>
2024-06-17 10:04:23 +0800

    added reply count for post

--- public/js/discussionforumscript.js
+++ public/js/discussionforumscript.js
@@ -29,6 +29,9 @@ async function fetchPosts() {
         accountElement.classList.add("account");
         accountElement.textContent = await fetchAccountName(post.accId);
 
+        // fetch reply count
+        const replyCount = await fetchReplyCount(post.postId);
+
         const dateTimeElement = document.createElement("div");
         dateTimeElement.classList.add("datetime");
         // Format the date and time
@@ -41,7 +44,8 @@ async function fetchPosts() {
         const formattedDate = `${day}/${month}/${year}`;
         const formattedTime = `${hours}:${minutes}`;
 
-        dateTimeElement.textContent = `${formattedDate}, ${formattedTime}`;
+        // display reply count and datetime
+        dateTimeElement.innerHTML = `<i class="bi bi-chat-dots-fill"></i>  ${replyCount} | ${formattedDate}, ${formattedTime}`;
 
         const textElement = document.createElement("div");
         textElement.classList.add("text");
@@ -50,7 +54,6 @@ async function fetchPosts() {
         // header to hold account and datetime
         const headerElement = document.createElement("div");
         headerElement.classList.add("boxheader");
-
 
         // ... add more elements for other book data (optional)
 
@@ -69,6 +72,13 @@ async function fetchAccountName(accId) {
     const response = await fetch(`/accounts/${accId}`);
     const account = await response.json();
     return account.accName;
+}
+
+// function to fetch reply count for a post
+async function fetchReplyCount(postId) {
+    const response = await fetch(`/posts/${postId}/replyCount`);
+    const replyCount = await response.json();
+    return replyCount.replyCount;
 }
 
 fetchPosts(); // Call the function to fetch and display book data
