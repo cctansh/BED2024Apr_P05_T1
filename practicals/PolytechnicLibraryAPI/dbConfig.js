4b2db2b9adb2947c6b6f26891e31ca0cd114b500
cctansh <s10260078@connect.np.edu.sg>
2024-07-03 13:51:21 +0800

    added db .env to library

--- practicals/PolytechnicLibraryAPI/dbConfig.js
+++ practicals/PolytechnicLibraryAPI/dbConfig.js
@@ -1,11 +1,15 @@
-module.exports = {
-    user: "bed", 
-    password: "bed", 
-    server: "localhost",
-    database: "BEDAssignmentLibrary",
+require('dotenv').config();
+
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
     trustServerCertificate: true,
     options: {
-        port: 1433, 
-        connectionTimeout: 60000, 
+        port: 1433,
+        connectionTimeout: 60000,
     },
 };
+
+module.exports = dbConfig;
